0ce6f2af3817b6d6f76b1c50150f5b36a6432e57
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-02-28 21:46:14 +0530

    udemy lec 31.Server setup to 39.create update user to mongodb

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { readdirSync } = require('fs')
+require('dotenv').config()
+
+//app
+const app = express()
+
+//db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true
+    })
+    .then(() => console.log('DB CONNECTED'))
+    .catch(err => console.log(`DB CONNECTION ERR ${err}`))
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+//import routes
+//const authRoutes = require('./routes/auth')
+//routes middleware
+// app.use('/api', authRoutes)
+//^^^ no need to get routes like above codes,bcoz we r dynamically getting all routes from below code ^^^
+readdirSync('./routes').map((r) =>
+    app.use('/api', require('./routes/' + r))
+)
+
+//port
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
