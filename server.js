5365ad3dbf3c46922dc657fb7486190b7e2e64f5
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-10 21:04:31 +0530

    90. Multiple Image Uploads - cors, PayloadTooLargeError

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { readdirSync } = require('fs')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
 
 //app
-const app = express()
+const app = express();
 
 //db
 mongoose
-    .connect(process.env.DATABASE, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: true
-    })
-    .then(() => console.log('DB CONNECTED'))
-    .catch(err => console.log(`DB CONNECTION ERR ${err}`))
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(`DB CONNECTION ERR ${err}`));
 
 //middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(cors());
 
 //import routes
 //const authRoutes = require('./routes/auth')
 //routes middleware
 // app.use('/api', authRoutes)
 //^^^ no need to get routes like above codes,bcoz we r dynamically getting all routes from below code ^^^
-readdirSync('./routes').map((r) =>
-    app.use('/api', require('./routes/' + r))
-)
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 //port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
