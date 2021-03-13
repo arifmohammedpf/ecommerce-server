4769cc71d04371d357b3730fa9ae541c8b4653a8
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-13 21:44:28 +0530

    110. Update A Product

--- controllers/product.js
+++ controllers/product.js
@@ -60,6 +60,9 @@ exports.update = async (req, res) => {
     res.json(updated);
   } catch (err) {
     console.log("PRODUCT UPDATE ERROR--->", err);
-    return res.status(400).send("Product update failed");
+    // return res.status(400).send("Product update failed");
+    res.status(400).json({
+      err: err.message,
+    });
   }
 };
