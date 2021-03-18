f091e2810298f7859c110ff59db1d2bd8edc43f2
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-18 08:50:09 +0530

    typo in router for list products

--- routes/product.js
+++ routes/product.js
@@ -21,6 +21,6 @@ router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
 
-router.post("products", list);
+router.post("/products", list);
 
 module.exports = router;
